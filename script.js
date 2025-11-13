33424c8a823274dc3847b9f8e381c6c0402643d5
Aleksandre10 <kamladze.alex2@gmail.com>
2025-11-13 10:01:51 +0400

    created html,css and js files

---
+++ script.js
@@ -0,0 +1,38 @@
+let number = 15;
+let square= Math.pow(number,2)
+
+if (square > 100) {
+    console.log("მეტია ასზე");
+} else {
+console.log("ნაკლებია ასზე");
+}
+
+let number2 = 65;
+
+if (number2 > 50 && number2 < 100) {
+    console.log("რიცხვი 50-ზე მეტია და 100-ზე ნაკლებია");
+} else {
+    console.log("რიცხვი 50-ზე ნაკლებია ან 100-ზე მეტი");
+}
+
+let number3 = -20;
+
+if (number3 === 0) {
+  console.log("რიცხვი ნულის ტოლია");
+} else if (number3 < 0) {
+  console.log("რიცხვი უარყოფითია");
+} else {
+  console.log("რიცხვი დადებითია");
+}
+
+let lenght = 10
+let width = 12
+
+let perimeter = 2 * ( lenght + width)
+
+if (perimeter > 50) {
+    console.log("პერიმეტრი არის 50-ზე მეტი");
+} else {
+    console.log("პერიმეტრი არის 50-ზე ნაკლები ან ტოლი");
+    
+}
